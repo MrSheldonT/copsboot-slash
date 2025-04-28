8c7d212be3b09b394717b01cc4dc5d68082c0999
MrSheldonT <kukuo798@gmail.com>
2025-04-27 20:28:22 -0600

    Unit test for a built-in validator

--- src/test/java/com/slash/copsboot/user/web/UserRestControllerTest.java
+++ src/test/java/com/slash/copsboot/user/web/UserRestControllerTest.java
@@ -81,4 +81,21 @@ public class UserRestControllerTest {
                 .andExpect(status().isForbidden());
     }
 
+    @Test
+    void givenEmptyMobileToken_badRequestIsReturned() throws Exception {
+        mockMvc.perform(post("/api/users")
+                        .with(jwt().jwt(builder -> builder.subject(UUID.randomUUID().toString()))
+                                .authorities(new SimpleGrantedAuthority("ROLE_OFFICER")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "mobileToken": ""
+                                }
+                                """))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+
+        verify(userService, never()).createUser(any(CreateUserParameters.class));
+    }
+
 }
